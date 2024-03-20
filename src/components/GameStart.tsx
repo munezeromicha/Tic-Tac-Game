ae208c4a60c9cf5dc54679bd74c7446273277646
munezeromicha <munezerontaganiramichel@gmail.com>
2024-03-20 14:47:02 +0200

    Tic-Tac-Game~updates: Responsiveness added on all pages

--- src/components/GameStart.tsx
+++ src/components/GameStart.tsx
@@ -6,35 +6,35 @@ import { useNavigate } from 'react-router-dom';
 export const GameStart = () => {
   const navigate = useNavigate();
   return (
-    <div className='container'>
-    <div className='top'>
-        <img src={logo} alt="logo" className='logo'/>
-        <div>
-        <button className='btn-1' onClick={()=>navigate('/return')}><img src={iconx} alt="" className='img-2'/><h3>TURN</h3></button>
-        </div>
-        <div>
-        <button className='btn-2'><img src={restart} alt="" /></button>
-        </div>
+    <div className='container-start'>
+    <div className='top-start'>
+        <img src={logo} alt="logo" className='logo-start'/>
+    
+        <button className='btn-1-start' onClick={()=>navigate('/return')}><img src={iconx} alt="" className='img-2-start'/><h3>TURN</h3></button>
+        
+    
+        <button className='btn-2-start'><img src={restart} alt="" /></button>
+        
     </div>
-    <div className='middle'>
-        <button className='mid-btn'></button>
-        <button className='mid-btn' ></button>
-        <button className='mid-btn'></button>
+    <div className='middle-start'>
+        <button className='mid-btn-start'></button>
+        <button className='mid-btn-start' ></button>
+        <button className='mid-btn-start'></button>
     </div>
-    <div className='middle-2'>
-        <button className='mid-btn'></button>
-        <button className='mid-btn'></button>
-        <button className='mid-btn'></button>
+    <div className='middle-2-start'>
+        <button className='mid-btn-start'></button>
+        <button className='mid-btn-start'></button>
+        <button className='mid-btn-start'></button>
     </div>
-    <div className='middle-3'>
-        <button className='mid-btn'></button>
-        <button className='mid-btn'></button>
-        <button className='mid-btn'></button>
+    <div className='middle-3-start'>
+        <button className='mid-btn-start'></button>
+        <button className='mid-btn-start'></button>
+        <button className='mid-btn-start'></button>
     </div>
-    <div className='bottom'>
-        <button className='mid-btn-11'><span className='inside-btn'><p className='text-1'>X (YOU)</p><p className='text-2'>0</p></span></button>
-        <button className='mid-btn-12'><span className='inside-btn'><p className='text-1'>TIES</p><p className='text-2'>0</p></span></button>
-        <button className='mid-btn-13'><span className='inside-btn'><p className='text-1'>O (CPU)</p><p className='text-2'>0</p></span></button>
+    <div className='bottom-start'>
+        <button className='mid-btn-11-start'><span className='inside-btn-start'><p className='text-1-start'>X (YOU)</p><p className='text-2-start'>0</p></span></button>
+        <button className='mid-btn-12-start'><span className='inside-btn-start'><p className='text-1-start'>TIES</p><p className='text-2-start'>0</p></span></button>
+        <button className='mid-btn-13-start'><span className='inside-btn-start'><p className='text-1-start'>O (CPU)</p><p className='text-2-start'>0</p></span></button>
     </div>
 </div>
   )
